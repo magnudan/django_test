13b9aa235a66a4deb5da2d5faaf41d75f8e8a898
Magnus Danielsen <magnudan@stud.ntnu.no>
2019-02-24 16:46:26 +0100

    One more thing

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        'author':   'CoreyMS',
+        'title':    'Blog Post 1',
+        'content':  'First post conent',
+        'date_posted': '24.02.19'
+    }
+
+]
+
 def home(request):
     return render(request, 'blog/home.html', {})
 
